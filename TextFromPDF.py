7ae978585413a7cddada8c59452c434c7a9a1f59
Michel Bovender <mbovender@gmail.com>
2024-03-15 07:06:17 +0100

    🐛: Fixed the version conflict. Now PyPDF2 is for Python 3.

--- TextFromPDF.py
+++ TextFromPDF.py
@@ -3,19 +3,25 @@ import PyPDF2
 
 text = []
 def extract_text_from_pdf(pdf_path):
+    # Check if the file exists
     if os.path.exists(pdf_path):
+        # open the pdf
         with open(pdf_path, 'rb') as f:
-            ### TODO: bug fix here!
-            pdf_reader = PyPDF2.PdfFileReader(f)
-            for page_number in range(pdf_reader.numPages):
-                page = pdf_reader.getPage(page_number)
-                text.append(page.extractText())
+            pdf_reader = PyPDF2.PdfReader(f)
+            # Get the text of every page
+            for page_number in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_number]
+                text.append(page.extract_text())
     else:
-        print("File does not exist")
+        print("File does not exist.")
 
+#pdf_path = "files/example.pdf"
 pdf_path = "files/spin.pdf"
 
 extract_text_from_pdf(pdf_path)
 if len(text) > 0:
-    for t in text:
-        print(text[t])
+    counter = 1
+    for text in text:
+        print("\n#### Page", counter, "####")
+        print(text)
+        counter += 1
